b6de1880743a06406f66dc89a53363be64e450f3
p. schuhmacher <contact@pschuhmacher.com>
2024-09-25 18:15:19 +0200

    mise a jour

---
+++ module-02-tutoriel-premiers-pas/demo-live/README.md
@@ -0,0 +1,3 @@
+# Langage C - Module 02
+
+Les sources des démos faites en live pendant les cours.
---
+++ module-02-tutoriel-premiers-pas/demo-live/char-ascii-division-entiere.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // char c = 'A';
+    // printf("%c\n", c);
+    // printf("%d\n", c);
+
+    // c = c - 1;
+    // //Affiche le caractère ASCII correspondant
+    // printf("%c\n", c);
+    // //Affiche sa valeur décimale
+    // printf("%d\n", c);
+
+    // return 0;
+
+    //Division entière : quel est le résultat q ?
+    int n = 5;
+    int m = 3;
+    int q = n / m;
+    printf("%d\n", q);
+
+
+
+}

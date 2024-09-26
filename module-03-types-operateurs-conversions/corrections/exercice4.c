f079f1066bb746175a7bcff8044f397dc2a755bc
p. schuhmacher <contact@pschuhmacher.com>
2024-09-26 15:58:41 +0200

    ajoute commentaires

--- module-03-types-operateurs-conversions/corrections/exercice4.c
+++ module-03-types-operateurs-conversions/corrections/exercice4.c
@@ -2,9 +2,14 @@
 
 int main(){
 
-    int width = 10;
 
-    printf("This (\") is a quote, this (%) is a percent and this (\\) is a backslash.\n");
-    printf("[%*s]\n", width, "Foo bar");
+    //Pour échapper un caractère spécial (qu'il ne soit pas interprété), il faut utiliser le backslash
+    //Pour afficher le caractère '%' il faut le doubler '%%'
+    printf("This (\") is a quote, this %% is a percent and this (\\) is a backslash.\n");
+
+    //Tip: il est possible de paramétrer la largeur du spécificateur de format avec le caractère '*'
+    //et en passant la valeur de la largeur en argument de printf
+    int width = 10;
+    printf("[%*s]\n", width, "Foo");
     return 0;
 }

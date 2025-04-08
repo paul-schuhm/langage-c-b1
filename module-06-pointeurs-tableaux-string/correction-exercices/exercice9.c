cd8ba9f86f45e8a1731fab7866628702fa7b3775
p schuhmacher <contact@pschuhmacher.com>
2025-04-08 13:12:45 +0200

    mise a jour

--- module-06-pointeurs-tableaux-string/correction-exercices/exercice9.c
+++ module-06-pointeurs-tableaux-string/correction-exercices/exercice9.c
@@ -1,35 +1,48 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-#define MAX 31 // 30 caractères + '\0'
+#define LGMAX 31
 
-int main()
-{
-    char word[MAX];
-    int i, len, is_palindrome = 1;
+int main(){
 
-    printf("entrez un mot (max 30 caractères) : ");
-    scanf("%30s", word); // empêche le dépassement de tampon
+	//N'initialise pas une chaine vide. Reserve mémoire et du gargage dedans !
+	char line[LGMAX];
+	//Initialise une chaine vide si besoin
+	//char line[LGMAX] = {0};
 
-    len = strlen(word);
+	printf("tapez une phrase, pressez return pour terminer la saisie : ");
+	fgets(line, LGMAX, stdin);
+	//Si user tape 'hello\n', la chaine line contriendra 'hello\n\0'
 
-    printf("mot inversé : ");
-    for (i = len - 1; i >= 0; i--)
-        putchar(word[i]);
-    putchar('\n');
+	// Supprimer le caractère \n, s'il est présent (c'est le cas avec fgets, il ne le consomme pas)
+	line[strcspn(line, "\n")] = '\0';
 
-    //Solution par symetrie
-    for (i = 0; i < len / 2; i++)
-    {
-        if (word[i] != word[len - 1 - i])
-        {
-            is_palindrome = 0;
-            break;
-        }
-    }
+	//Renvoie la taille de la chaine 
+	//strlen() ne compte pas les bytes alloués mais la longueur réelle de la chaîne, c'est-à-dire le nombre de caractères avant le caractère nul (\0), qui est ajouté automatiquement par fgets() à la fin de la chaîne.
+	int len = strlen(line);
 
-    if (is_palindrome)
-        printf("le mot %s est un palindrome.\n", word);
+	//Inverser la chaine
+	for(int i = len - 1 ; i >= 0 ; i--)
+		printf("%c", line[i]);
+	//Palindrome
+	//Méthode par symétrie
 
-    return 0;
+    //On assume que c'est un palindrome par défaut.
+	int is_palindrome = 1;
+
+	for(int i = 0; i < len/2; i++){
+		if(line[i] != line[len - 1 - i]){
+			//Manque de symetrie autour du centre : pas un palindrome !
+			is_palindrome = 0;
+			//Je peux arrêter tout de suite la boucle.
+			break;
+		}
+	}
+
+	printf("\n");
+
+	if(is_palindrome)
+		printf("%s est un palindrome !\n", line);
+
+	printf("\n");
 }
